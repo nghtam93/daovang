49531af031c66b39d6b5cb9529666b860c95d72a
doannguyen263 <doannguyen263@gmail.com>
2021-10-13 21:19:03 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,28 +18,31 @@ $(function() {
 			dots: true,
 			easing: "linear"
 		});
+
+		$('.home-discover__slider').slick({
+			centerMode: true,
+  			centerPadding: '0',
+		  	infinite: true,
+		  	slidesToShow: 3,
+		  	slidesToScroll: 3
+		});
+
+	}
+	if($('body').hasClass('maps')){
+		$('#mapTab').on("click",function(e) {
+			var offset_top = $('.maps-content').offset().top;
+			console.log(offset_top)
+			$("html, body").animate({scrollTop:offset_top},500)
+		})
 	}
 
-	// $(".selected").hover(function () {
-	//       var hoverImg = HoverImgOf($(this).attr("src"));
-	//       $(this).attr("src", hoverImg);
-	// }, function () {
-	//       var normalImg = NormalImgOf($(this).attr("src"));
-	//       $(this).attr("src", normalImg);
-	// });
 
-	// function HoverImgOf(filename) {
-	//     var re = new RegExp("(.+)\\.(gif|png|jpg)", "g");
-	//     return filename.replace(re, "$1_hover.$2");
-	// }
-
-
-    $(window).scroll(function (){
-        const scroll = $(window).scrollTop();
-        $('.home-wellcome--door').css({
-            transform: 'translate3d(0, '+(scroll/10) +'%,0) scale('+(100 + scroll/5)/100+')'
-        })
-    });
+    // $(window).scroll(function (){
+    //     const scroll = $(window).scrollTop();
+    //     $('.home-wellcome--door').css({
+    //         transform: 'translate3d(0, '+(scroll/10) +'%,0) scale('+(100 + scroll/5)/100+')'
+    //     })
+    // });
 
 
 });
